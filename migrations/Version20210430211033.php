feb37437d4bc885c676bd0c98e742c32c745c27f
DrozdAllan <drozdallan@gmail.com>
2021-05-01 19:10:50 +0200

    test

--- migrations/Version20210430211033.php
+++ migrations/Version20210430211033.php
@@ -20,6 +20,7 @@ final class Version20210430211033 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user');
         $this->addSql('ALTER TABLE user ADD mail VARCHAR(255) NOT NULL, ADD vip DATETIME DEFAULT NULL, ADD stripe VARCHAR(255) DEFAULT NULL, ADD notif TINYINT(1) DEFAULT NULL');
         $this->addSql('CREATE TABLE channel (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE channel_user (channel_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_11C7753772F5A1AA (channel_id), INDEX IDX_11C77537A76ED395 (user_id), PRIMARY KEY(channel_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
