91d90f64cf07b928475fca4d02644b2e29de632e
DrozdAllan <drozdallan@gmail.com>
2021-07-23 19:14:08 +0200

    changed the sass processor for phpStorm, added rounded bootstrap class

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -8,6 +8,8 @@ use App\Form\UploadImageType;
 use App\Service\ImageUploader;
 use App\Repository\ImageRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -15,66 +17,81 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ImageController extends AbstractController
 {
-    /**
-     * @Route("/profile/addimage", name="addImage")
-     */
-    public function addImage(Request $request, ImageUploader $imageUploader, EntityManagerInterface $em)
-    {
+  /**
+   * @Route("/profile/addimage", name="addImage")
+   */
+  public function addImage(Request $request, ImageUploader $imageUploader, EntityManagerInterface $em)
+  {
 
-        $image = new Image;
-        $user = $this->getUser();
+    $image = new Image;
+    $imagine = new Imagine();
+    $user = $this->getUser();
 
-        $form = $this->createForm(UploadImageType::class);
-        $form->handleRequest($request);
+    $form = $this->createForm(UploadImageType::class);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+    if ($form->isSubmitted()) {
 
-            /** @var UploadedFile $imageFile */
-            $imageFile = $form->get('image')->getData();
+      /** @var UploadedFile $imageFile */
+      $imageFile = $form->get('image')->getData();
 
-            if ($imageFile) {
+      if ($imageFile) {
 
-                /** @var User $user */
-                $imageFileName = $imageUploader->upload($imageFile);
-                $image->setImageFilename($imageFileName);
-
-                $user->addImage($image);
-                $em->persist($image);
-                $em->flush();
-                $this->addFlash("success", "Image ajoutée avec succès");
-            }
-
-            return $this->redirectToRoute('profile');
+        list($iwidth, $iheight) = getimagesize($imageFile);
+        $ratio = $iwidth / $iheight;
+        $width = 256;
+        $height = 294;
+        if ($width / $height > $ratio) {
+          $width = $height * $ratio;
+        } else {
+          $height = $width / $ratio;
         }
 
-        $formView = $form->createView();
+        $photo = $imagine->open($imageFile);
+        $photo->resize(new Box($width, $height))->save($imageFile);
 
-        return $this->render('profile/addImage.html.twig', [
-            'formView' => $formView
-        ]);
+
+        /** @var User $user */
+        $imageFileName = $imageUploader->upload($imageFile);
+        $image->setImageFilename($imageFileName);
+
+        $user->addImage($image);
+        $em->persist($image);
+        $em->flush();
+        $this->addFlash("success", "Image ajoutée avec succès");
+      }
+
+      return $this->redirectToRoute('profile');
     }
 
+    $formView = $form->createView();
 
-    /**
-     * @Route("/profile/deleteimage/{imageid}", name="announcerDeleteImage")
-     * Apparemment pas safe
-     */
-    public function announcerDeleteImage($imageid, ImageUploader $imageUploader, ImageRepository $imageRepository, EntityManagerInterface $em)
-    {
-        $image = $imageRepository->findOneBy(['id' => $imageid]);
+    return $this->render('profile/addImage.html.twig', [
+      'formView' => $formView
+    ]);
+  }
 
-        if ($image) {
 
-            // Supprimer image du dossier Public/Uploads
-           $imageUploader->delete($image);
+  /**
+   * @Route("/profile/deleteimage/{imageid}", name="announcerDeleteImage")
+   * Apparemment pas safe
+   */
+  public function announcerDeleteImage($imageid, ImageUploader $imageUploader, ImageRepository $imageRepository, EntityManagerInterface $em)
+  {
+    $image = $imageRepository->findOneBy(['id' => $imageid]);
 
-            // Supprimer image de la DB
-            $em->remove($image);
-            $em->flush();
-            $this->addFlash("success", "Image supprimée avec succès");
+    if ($image) {
 
-            //redirection vers le profil de l'announcer
-            return $this->redirectToRoute('modifyAnnouncerProfile');
-        }
+      // Supprimer image du dossier Public/Uploads
+      $imageUploader->delete($image);
+
+      // Supprimer image de la DB
+      $em->remove($image);
+      $em->flush();
+      $this->addFlash("success", "Image supprimée avec succès");
+
+      //redirection vers le profil de l'announcer
+      return $this->redirectToRoute('modifyAnnouncerProfile');
     }
+  }
 }
--- src/Form/UploadImageType.php
+++ src/Form/UploadImageType.php
@@ -4,48 +4,50 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 
-
 class UploadImageType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('image', FileType::class, [
-                'label' => 'image (jpeg/png)',
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('image', FileType::class, [
+        'label' => 'image (jpeg/png)',
 
-                // unmapped means that this field is not associated to any entity property
-                'mapped' => false,
+        // unmapped means that this field is not associated to any entity property
+        'mapped' => false,
 
-                // make it optional so you don't have to re-upload the PDF file
-                // every time you edit the Product details
-                'required' => false,
+        // make it optional so you don't have to re-upload the PDF file
+        // every time you edit the Product details
+        'required' => false,
 
-                // unmapped fields can't define their validation using annotations
-                // in the associated entity, so you can use the PHP constraint classes
-                'constraints' => [
-                    new File([
-                        'maxSize' => '1024k',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/png',
-                        ],
-                        'mimeTypesMessage' => 'Please upload a valid format image',
-                    ])
-                ],
-            ])
-        ;
-    }
+        // unmapped fields can't define their validation using annotations
+        // in the associated entity, so you can use the PHP constraint classes
+        'constraints' => [
+          new File([
+            'maxSize' => '1024k',
+            'mimeTypes' => [
+              'image/jpeg',
+              'image/png',
+            ],
+            'mimeTypesMessage' => 'Please upload a valid format image',
+          ])
+        ],
+      ])
+      ->add('submit', SubmitType::class, array(
+        'label' => 'Upload'
+      ));
+  }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Image::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults([
+      'data_class' => Image::class,
+    ]);
+  }
 }
